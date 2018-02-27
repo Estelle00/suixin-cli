3d37e405967d695b6db7e18e40b8aaaa45eec561
刘炳文 <liubingwen@huianrong.com>
2018-02-27 19:44:09 +0800

    修改执行add命令方式

---
+++ bin/har.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+const {resolve, join} = require('path')
+process.env.NODE_PATH = join(__dirname, '../node_modules/')
+const program = require('commander')
+
+program
+  .version(require('../package').version)
+
+program
+  .usage('<command>')
+
+program
+  .command('add')
+  .description('添加一个新的模板！')
+  .alias('a')
+  .action(() => {
+    require('../command/add')
+  })
+
+program
+  .command('list')
+  .description('列出所有可用模板！')
+  .alias('l')
+  .action(() => {
+    require('../command/list')()
+  })
+
+program
+  .command('init')
+  .description('初始化一个新的项目！')
+  .alias('i')
+  .action(() => {
+    require('../command/init')()
+  })
+
+program
+  .command('delete')
+  .description('删除一个模板！')
+  .alias('d')
+  .action(() => {
+    require('../command/delete')()
+  })
+
+program.parse(process.argv)
+
+if (!program.args.length) {
+  program.help()
+}
