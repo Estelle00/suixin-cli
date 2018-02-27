b1b58d7eff9d9d915749a2b8f7af64f6185ce140
刘炳文 <liubingwen@huianrong.com>
2018-02-27 18:29:52 +0800

    改造添加模板信息模块

--- command/add.js
+++ command/add.js
@@ -1,34 +1,49 @@
 /**
  * Created by liubingwen on 2018/2/27.
  */
-const fs = require('fs')
-const path = require('path')
-const co = require('co')
-const prompt = require('co-prompt')
-const chalk = require('chalk')
-const config = require('../templates')
-module.exports = () => {
-  co(function * () {
-    const tplName = yield prompt('Template name: ')
-    if (config[tplName]) {
-      console.log('当前模板已存在！')
-      process.exit()
+const {prompt} = require('inquirer')
+const {writeFile} = require('fs')
+const {listTable} = require('../utils/index')
+const tplList = require('../templates')
+const {TEMPLATES_PATH} = require('../config/index')
+const question = [
+  {
+    type: 'input',
+    name: 'name',
+    message: '请输入添加的模板名称：',
+    validate (val) {
+      if (tplList[val]) {
+        return '模板已经存在！'
+      } else if (val === '') {
+        return '模板名称不能为空！'
+      }
+      return true
     }
-    const gitUrl = yield prompt('Git https link: ')
-    const branch = yield prompt('Branch: ')
-    config[tplName] = {}
-    config[tplName]['url'] = gitUrl.replace(/[\u0000-\u0019]/g, '') // 过滤unicode字符
-    config[tplName]['branch'] = branch
-    // 模板信息写入templates.json文件
-    const templatesPath = path.resolve(__dirname, '../templates.json')
-    console.log('templatesPath', templatesPath)
-    fs.writeFile(templatesPath, JSON.stringify(config), 'utf-8', err => {
-      if (err) chalk.red(err)
-      console.log(chalk.green('模板添加成功！!\n'))
-      console.log(chalk.grey('已添加模板列表如下: \n'))
-      console.log(config)
-      console.log('\n')
-      process.exit()
-    })
+  },
+  {
+    type: 'input',
+    name: 'place',
+    message: '请添加owner/name：',
+    validate (val) {
+      if (val !== '') {
+        return true
+      }
+      return '请填写owner/name！'
+    }
+  },
+  {
+    type: 'input',
+    name: 'branch',
+    message: '请填写git分支(master)：',
+    default: 'master'
+  }
+]
+module.exports = prompt(question).then(({name, place, branch}) => {
+  tplList[name] = {}
+  tplList[name]['owner/name'] = place
+  tplList[name]['branch'] = branch
+  writeFile(TEMPLATES_PATH, JSON.stringify(tplList), 'utf-8', err => {
+    if (err) console.log(err)
+    listTable(tplList, '新的模板添加成功！')
   })
-}
+})
