69cec8f1c0450f7072f51e0fae2e9348844ae3e8
刘炳文 <liubingwen@huianrong.com>
2018-02-28 15:05:29 +0800

    添加克隆git目录已存在时清除目录

--- command/init.js
+++ command/init.js
@@ -2,6 +2,8 @@
  * Created by liubingwen on 2018/2/27.
  */
 const { prompt } = require('inquirer')
+const exists = require('fs').existsSync
+const rm = require('rimraf').sync
 const home = require('user-home')
 const path = require('path')
 const chalk = require('chalk')
@@ -9,6 +11,7 @@ const download = require('download-git-repo')
 const ora = require('ora')
 const tplList = require('../templates')
 const {getGitList} = require('../lib/index')
+const generate = require('../lib/generate')
 const errorLog = (lyric) => {
   chalk.red(lyric)
   process.exit()
@@ -35,16 +38,20 @@ module.exports = async (project) => {
   prompt(questions).then(({name, place}) => {
     const gitPlace = newList[name]['owner/name']
     const gitBranch = newList[name]['branch']
+    const tmp = path.join(home, '.har-templates', name.replace(/[/:]/g, '-'))
     const spinner = ora('模板下载中...')
     spinner.start()
-    const tmp = path.join(home, '.har-templates', name.replace(/[/:]/g, '-'))
+    if (exists(tmp)) rm(tmp)
     download(`${gitPlace}#${gitBranch}`, tmp, {clone: true}, err => {
+      spinner.stop()
       if (err) {
         console.log(chalk.red(err))
         process.exit()
       }
-      spinner.stop()
-      console.log(chalk.green('项目初始化成功！'))
+      generate(project, tmp, place, err => {
+        if (err) console.log(chalk.red(err))
+        console.log(chalk.green('项目初始化成功！'))
+      })
     })
   })
 }
