f3a1233064375fe7612f3201a6c9d2e4beb849dd
刘炳文 <liubingwen@huianrong.com>
2018-03-05 13:51:12 +0800

    调整api接口地址到配置文件

--- config/index.js
+++ config/index.js
@@ -5,5 +5,7 @@ const path = require('path')
 const ROOT_PATH = path.resolve(__dirname, '../')
 module.exports = {
   ROOT_PATH,
-  TEMPLATES_PATH: path.resolve(ROOT_PATH, './templates.json')
+  TEMPLATES_PATH: path.resolve(ROOT_PATH, './templates.json'),
+  cliTagUrl: 'http://192.168.3.239:4873/-/package/har-cli/dist-tags',
+  templateUrl: 'http://192.168.3.200:10080/api/v4/groups/har-templates/projects?private_token=cuKAsR7EYcyJrmNGzmkX'
 }
